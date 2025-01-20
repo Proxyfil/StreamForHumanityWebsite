aaa0ca277c0922922c6e522c22425d58249644d7
Proxyfil <piloupilou200@gmail.com>
2025-01-20 01:46:21 +0100

    feat : first commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Navbar from './components/Navbar.vue'
+</script>
+
+<script>
+export default {
+  name: 'app',
+  components: {
+    Navbar,
+  },
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<template>
+  <Navbar />
+  <RouterView />
+</template>
+
+<style scoped>
+main {
+  min-height: 100vh;
+  background-color: #101010A0;
+}
+
+::-webkit-scrollbar {
+  width: 5px;
+}
+::-webkit-scrollbar-track {
+  background: #020202;
+}
+::-webkit-scrollbar-thumb {
+  border-radius: 4px;
+  background: #808080;
+}
+::-webkit-scrollbar-thumb:hover {
+  background: #808080;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  name: 'app',
+  components: {
+
+  },
+  data() {
+    return {
+      selected: "home"
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <div class="text-xl p-3 flex justify-between fixed top-0 w-full z-20" id="nav-container">
+      <a href="https://humanity.gdoc.fr"><p class="font-semibold">Retour au GDoc</p></a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#nav-container {
+  backdrop-filter: blur(8px);
+  background-color: #101010A0;
+}
+</style>
